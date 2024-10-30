909509bf367afa16d71a947de5c104e9e65184e6
schwaad <joseivoschwadea@gmail.com>
2024-10-29 21:29:01 -0300

    New Backup

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     eval (ssh-agent -c)
-    ssh-add ~/.ssh/id_ed25519
+    ~/Scripts/autosshpass.sh
+    clear
     alias reloadwaybar='killall waybar && nohup waybar & && rm nohup.out'
     set fish_greeting ""
     cat /home/schwaad/.cache/wal/sequences
