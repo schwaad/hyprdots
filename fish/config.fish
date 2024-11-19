780118581f2c2671d09bff88640eb24e4a020e5d
schwaad <joseivoschwadea@gmail.com>
2024-11-19 17:07:40 -0300

    New Backup

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     eval (ssh-agent -c)
     ~/Scripts/autosshpass.sh
+    source ~/.config/fish/functions/marcopolo.fish
     clear
     alias reloadwaybar='killall waybar && nohup waybar & && rm nohup.out'
+    alias open-mysqlWorkbench='nohup mysql-workbench & && rm nohup.out'
     set fish_greeting ""
     cat /home/schwaad/.cache/wal/sequences
     ~/.cargo/bin/pokeget random --hide-name
 end
+
+fish_add_path /home/schwaad/.spicetify
