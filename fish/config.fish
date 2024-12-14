bf1a3918abf7248de7639263d961b171fdeed2dc
schwaad <joseivoschwadea@gmail.com>
2024-12-14 19:34:15 -0300

    New Backup

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,7 @@ if status is-interactive
     clear
     alias reloadwaybar='killall waybar && nohup waybar & && rm nohup.out'
     alias open-mysqlWorkbench='nohup mysql-workbench & && rm nohup.out'
+    alias android-studio='nohup studio & && rm nohup.out'
     set fish_greeting ""
     cat /home/schwaad/.cache/wal/sequences
     ~/.cargo/bin/pokeget random --hide-name
